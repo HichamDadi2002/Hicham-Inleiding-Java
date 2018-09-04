db1dda1de381a9fc3dd1cf56554fe01f65c873a7
HichamDadi2002 <42801914+HichamDadi2002@users.noreply.github.com>
2018-09-04 13:44:15 +0200

    V1.0

--- src/H1/H1_1.java
+++ src/H1/H1_1.java
@@ -1,4 +1,17 @@
 package H1;
 
-public class H1_1 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class H1_1 extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.drawString("Welcome to Java!!", 50, 60 );
+    }
+
 }
